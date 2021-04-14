f01837c688fc8f32ae828a52f48f21095d6f1900
Dawon Gregory <dawongregory@gmail.com>
2021-04-14 01:16:13 -0600

    added SportsORM 1 project

---
+++ ORM/SportsORM/SportsORM/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsORM.Models;
+
+
+namespace SportsORM.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static Context _context;
+
+        public HomeController(Context DBContext)
+        {
+            _context = DBContext;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            ViewBag.BaseballLeagues = _context.Leagues
+                .Where(l => l.Sport.Contains("Baseball"))
+                .ToList();
+            return View();
+        }
+
+        [HttpGet("level_1")]
+        public IActionResult Level1()
+        {
+            /////////////////////////////////////LEAGUES\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            ViewBag.WomenLeagues = _context.Leagues.Where(w => w.Name.Contains("Women")).ToList();
+            ViewBag.HockeyLeagues = _context.Leagues.Where(h => h.Sport.Contains("Hockey")).ToList();
+            ViewBag.OtherLeagues = _context.Leagues.Where(o => o.Sport != "Football").ToList();
+            ViewBag.ConferenceLeagues = _context.Leagues.Where(c => c.Name.Contains("Conference")).ToList();
+            ViewBag.AtlanticLeagues = _context.Leagues.Where(a => a.Name.Contains("Atlantic")).ToList();
+
+            //////////////////////////////////////TEAMS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            ViewBag.DallasTeams = _context.Teams.Where(d => d.Location.Contains("Dallas")).ToList();
+            ViewBag.RaptorTeams = _context.Teams.Where(r => r.TeamName.Contains("Raptors")).ToList();
+            ViewBag.CityTeams = _context.Teams.Where(c => c.Location.Contains("City")).ToList();
+            ViewBag.StartsWithTTeams = _context.Teams.Where(swt => swt.TeamName.StartsWith("T")).ToList();
+            ViewBag.LocationAToZTeams = _context.Teams.OrderBy(latz => latz.Location).ToList();
+            ViewBag.NameZToATeams = _context.Teams.OrderByDescending(nzta => nzta.TeamName).ToList();
+
+            /////////////////////////////////////PLAYERS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            ViewBag.LastNameCooperPlayers = _context.Players.Where(lnc => lnc.LastName.Contains("Cooper")).ToList();
+            ViewBag.FirstNameJoshuaPlayers = _context.Players.Where(fnj => fnj.FirstName.Contains("Joshua")).ToList();
+            ViewBag.LastNameCooperNotFirstNameJoshuaPlayers = _context.Players.Where(lncnfnj => lncnfnj.LastName == "Cooper").Where(lncnfnj => lncnfnj.FirstName != "Joshua").ToList();
+            ViewBag.FirstNameAlexanderOrFirstNameWyattPlayers = _context.Players.Where(fnaofnw => fnaofnw.FirstName == "Alexander" || fnaofnw.FirstName == "Wyatt").ToList();
+            return View();
+        }
+
+        [HttpGet("level_2")]
+        public IActionResult Level2()
+        {
+            return View();
+        }
+
+        [HttpGet("level_3")]
+        public IActionResult Level3()
+        {
+            return View();
+        }
+
+    }
+}
