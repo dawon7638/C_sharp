9afb1ad1ae3bba2d3297410bd5da9ab1c4d5178a
Dawon Gregory <dawongregory@gmail.com>
2021-04-16 23:26:54 -0600

    Added Sports ORM Lvl2 (only 2 of 8).

--- ORM/SportsORM/SportsORM/Controllers/HomeController.cs
+++ ORM/SportsORM/SportsORM/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SportsORM.Models;
 
 
@@ -31,14 +32,14 @@ namespace SportsORM.Controllers
         [HttpGet("level_1")]
         public IActionResult Level1()
         {
-            /////////////////////////////////////LEAGUES\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            /////////////////////////////////////LEAGUES LVL1\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
             ViewBag.WomenLeagues = _context.Leagues.Where(w => w.Name.Contains("Women")).ToList();
             ViewBag.HockeyLeagues = _context.Leagues.Where(h => h.Sport.Contains("Hockey")).ToList();
             ViewBag.OtherLeagues = _context.Leagues.Where(o => o.Sport != "Football").ToList();
             ViewBag.ConferenceLeagues = _context.Leagues.Where(c => c.Name.Contains("Conference")).ToList();
             ViewBag.AtlanticLeagues = _context.Leagues.Where(a => a.Name.Contains("Atlantic")).ToList();
 
-            //////////////////////////////////////TEAMS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            //////////////////////////////////////TEAMS LVL1\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
             ViewBag.DallasTeams = _context.Teams.Where(d => d.Location.Contains("Dallas")).ToList();
             ViewBag.RaptorTeams = _context.Teams.Where(r => r.TeamName.Contains("Raptors")).ToList();
             ViewBag.CityTeams = _context.Teams.Where(c => c.Location.Contains("City")).ToList();
@@ -46,7 +47,7 @@ namespace SportsORM.Controllers
             ViewBag.LocationAToZTeams = _context.Teams.OrderBy(latz => latz.Location).ToList();
             ViewBag.NameZToATeams = _context.Teams.OrderByDescending(nzta => nzta.TeamName).ToList();
 
-            /////////////////////////////////////PLAYERS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            /////////////////////////////////////PLAYERS LVL1\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
             ViewBag.LastNameCooperPlayers = _context.Players.Where(lnc => lnc.LastName.Contains("Cooper")).ToList();
             ViewBag.FirstNameJoshuaPlayers = _context.Players.Where(fnj => fnj.FirstName.Contains("Joshua")).ToList();
             ViewBag.LastNameCooperNotFirstNameJoshuaPlayers = _context.Players.Where(lncnfnj => lncnfnj.LastName == "Cooper").Where(lncnfnj => lncnfnj.FirstName != "Joshua").ToList();
@@ -57,6 +58,13 @@ namespace SportsORM.Controllers
         [HttpGet("level_2")]
         public IActionResult Level2()
         {
+            //////////////////////////////////////TEAMS LVL2\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            ViewBag.AllTeamsInAtlanticConference = _context.Teams.Include(atiac => atiac.CurrLeague).Where(atiac => atiac.CurrLeague.Name == ("Atlantic Soccer Conference")).ToList();
+
+            /////////////////////////////////////PLAYERS LVL2\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            ViewBag.BostonPlayers = _context.Players.Include(bp => bp.CurrentTeam).Where(bp => bp.CurrentTeam.TeamName == "Penguins" && bp.CurrentTeam.Location =="Boston").ToList();
+
+
             return View();
         }
 
